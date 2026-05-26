1f829d2b5b01d35f545df261321ce8eb8811ef40
youjunwang090102 <youjunwangdeapple@gmail.com>
2026-05-26 14:56:39 +0800

    Update firebase-config.js

--- home/firebase-config.js
+++ home/firebase-config.js
@@ -1,8 +1,7 @@
-// 檔案：firebase-config.js
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.13.0/firebase-app.js";
-import { getFirestore, doc, setDoc, onSnapshot } from "https://www.gstatic.com/firebasejs/12.13.0/firebase-firestore.js";
+// 這裡新增了 collection, addDoc, serverTimestamp, query, orderBy
+import { getFirestore, doc, setDoc, onSnapshot, collection, addDoc, serverTimestamp, query, orderBy } from "https://www.gstatic.com/firebasejs/12.13.0/firebase-firestore.js";
 
-// 你的專屬 Firebase Config
 const firebaseConfig = {
   apiKey: "AIzaSyC7YY86Pn0E1EkyXyPZYZYHvLj-bOZL60o",
   authDomain: "school-fair-pos-72758.firebaseapp.com",
@@ -12,9 +11,7 @@ const firebaseConfig = {
   appId: "1:517697201549:web:fd19f4480ecf4326d109c4"
 };
 
-// 初始化 Firebase
 const app = initializeApp(firebaseConfig);
-
-// 導出資料庫實體與需要的操作函式
 export const db = getFirestore(app);
-export { doc, setDoc, onSnapshot };
+// 記得把新工具也 export 出去
+export { doc, setDoc, onSnapshot, collection, addDoc, serverTimestamp, query, orderBy };
