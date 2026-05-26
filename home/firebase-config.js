a8318fa7e087b62b9a4654dbc114519cbcf38a5f
youjunwang090102 <youjunwangdeapple@gmail.com>
2026-05-26 14:48:28 +0800

    Update firebase-config.js

--- home/firebase-config.js
+++ home/firebase-config.js
@@ -1,17 +1,20 @@
 // 檔案：firebase-config.js
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-app.js";
-import { getFirestore, doc, setDoc, onSnapshot } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-firestore.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.13.0/firebase-app.js";
+import { getFirestore, doc, setDoc, onSnapshot } from "https://www.gstatic.com/firebasejs/12.13.0/firebase-firestore.js";
 
-// TODO: 請務必換成你自己的 Firebase Config 內容
+// 你的專屬 Firebase Config
 const firebaseConfig = {
-  apiKey: "AIzaSy_你的金鑰...",
-  authDomain: "你的專案.firebaseapp.com",
-  projectId: "你的專案ID",
-  storageBucket: "你的專案.appspot.com",
-  messagingSenderId: "123456789",
-  appId: "1:123456789:web:abcde"
+  apiKey: "AIzaSyC7YY86Pn0E1EkyXyPZYZYHvLj-bOZL60o",
+  authDomain: "school-fair-pos-72758.firebaseapp.com",
+  projectId: "school-fair-pos-72758",
+  storageBucket: "school-fair-pos-72758.firebasestorage.app",
+  messagingSenderId: "517697201549",
+  appId: "1:517697201549:web:fd19f4480ecf4326d109c4"
 };
 
+// 初始化 Firebase
 const app = initializeApp(firebaseConfig);
+
+// 導出資料庫實體與需要的操作函式
 export const db = getFirestore(app);
 export { doc, setDoc, onSnapshot };
